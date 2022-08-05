18f8de87fb7c9398edeff190704d7a78c13c602f
sizumita <sumito@izumita.com>
2022-08-05 11:44:29 +0900

    Improve tailwind

--- machikado_network_frontend/app/routes/index.tsx
+++ machikado_network_frontend/app/routes/index.tsx
@@ -1,32 +1,19 @@
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+      <div className={"container mx-auto"}>
+          <div className={"sm:my-4 md:my-16"}>
+              <div className={"flex justify-center"}>
+                  <img
+                      src={"/MachikadoNetwork.svg"}
+                      width={512}
+                      height={512}
+                      alt={"logo image"}
+                  />
+              </div>
+              <div className="sm:text-3xl md:text-6xl text-center font-bold">
+                  まちかどネットワーク
+              </div>
+          </div>
+      </div>
   );
 }
