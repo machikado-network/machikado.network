a5848df1e64a11075b8b3edab83d64705caeda32
Shiro Miminashi Nohara <s.nohara.2001@gmail.com>
2022-08-07 05:48:45 +0900

    IPアドレス体系を見直した

--- tinc_node/exchange_key_in_vagrant.sh
+++ tinc_node/exchange_key_in_vagrant.sh
@@ -6,4 +6,4 @@ fakeroot_key="$(vagrant ssh fakeroot -c '/usr/local/sbin/show_tinc_key.sh' 2>/de
 gateway_key="$(vagrant ssh gateway -c '/usr/local/sbin/show_tinc_key.sh' 2>/dev/null)"
 
 vagrant ssh fakeroot -c "sudo /usr/local/sbin/config_tinc.sh gateway ${gateway_key}" 2>/dev/null
-vagrant ssh gateway -c "sudo ADDRESS=192.168.56.254 PORT=655 /usr/local/sbin/config_tinc.sh fakeroot ${fakeroot_key}" 2>/dev/null
+vagrant ssh gateway -c "sudo ADDRESS=172.16.38.1 PORT=655 /usr/local/sbin/config_tinc.sh fakeroot ${fakeroot_key}" 2>/dev/null
