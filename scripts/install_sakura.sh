27d9d00e53951588317b91065e4be2a4eaa9ca07
sizumita <sumito@izumita.com>
2022-08-10 19:54:53 +0900

    [sakura] Remove Sakura

--- scripts/install_sakura.sh
+++ scripts/install_sakura.sh
@@ -1,24 +1,29 @@
+RELEASES_API="https://api.github.com/repos/machikado-network/sakura/releases/latest"
 INSTALL_PATH="${1:-.}"
 
 function install() {
-  URL="$(echo $1 | grep $2)"
-  echo $URL
+  URL="$(echo "${1}" | grep "${2}")"
+  echo "Download Sakura from ${URL}"
   rm -f "${INSTALL_PATH}/sakura"
-  curl -L -s $URL > "${INSTALL_PATH}/sakura"
+  if hash curl 2>/dev/null; then
+    curl -L -s "${URL}" > "${INSTALL_PATH}/sakura"
+  else
+    wget -q -r "${URL}" -o "${INSTALL_PATH}/sakura"
+  fi
   chmod +x "${INSTALL_PATH}/sakura"
   exit 0
 }
 
 if hash jq 2>/dev/null; then
-  RELEASES="$(curl -L -s https://api.github.com/repos/sizumita/machikado.network/releases/latest)}"
+  RELEASES="$(curl -L -s "${RELEASES_API}" || wget -O - "${RELEASES_API}")}"
   ARCH="$(uname -m)"
   OS="$(uname -s)"
-  URLS="$(echo $RELEASES | jq -r '.assets[].browser_download_url' 2>/dev/null)"
+  URLS="$(echo "${RELEASES}" | jq -r '.assets[].browser_download_url' 2>/dev/null)"
 
   case "${OS}-${ARCH}" in
-    "Linux-arm64" ) install $URLS aarch64-unknown-linux-gnu ;;
-    "Linux-X86_64" ) install $URLS x86_64-unknown-linux-gnu ;;
-    "Linux-armv7l" ) install $URLS armv7-unknown-linux-gnueabihf ;;
+    "Linux-arm64" ) install "${URLS}" aarch64-unknown-linux-gnu ;;
+    "Linux-X86_64" ) install "${URLS}" x86_64-unknown-linux-gnu ;;
+    "Linux-armv7l" ) install "${URLS}" armv7-unknown-linux-gnueabihf ;;
   esac
 
   echo "No Matched Architecture and OS."
